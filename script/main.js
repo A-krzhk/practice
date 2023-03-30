36f2f35a642a81f4f0e952dbd18cefdcf2995f32
Alena koryazhkina <alena-koryazhkina@mail.ru>
2023-03-31 01:04:13 +0300

    some visual changes

--- script/main.js
+++ script/main.js
@@ -3,13 +3,11 @@ const listDocs = document.querySelector('.table-content tbody');
 const renderData = (data) => {
     data.forEach((el, i) => {
         listDocs.insertAdjacentHTML('beforeend',`
-            <tr class="cursor-pointer hover:bg-sky-100" data-id=${el.id}>
-                <td class="rounded-3xl">${i+1}</td>
-                <td class="rounded-3xl">${el.name}</td>
-                <td class="rounded-3xl">${el.data}</td>
-                <td class="rounded-3xl">${el.size}</td>
-                <td class="rounded-3xl">${el.author}</td>
-                <td class="rounded-3xl">${el.includeImg ? 'Да':'Нет'}</td>
+            <tr class="cursor-pointer hover:bg-gray-300 " data-id=${el.id}>
+                <td class="p-1">${el.name}</td>
+                <td class="p-1">${el.data}</td>
+                <td class="p-1">${el.size} ${el.includeImg ? '<i class="fa-regular fa-file-image text-gray-600"></i>' : ''}</td>
+                <td class="p-1">${el.author}</td>
             </tr>
         `);
     });
