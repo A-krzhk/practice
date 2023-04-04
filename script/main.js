3197e402c80559156036b03009af000a8e71b902
Alena koryazhkina <alena-koryazhkina@mail.ru>
2023-04-04 17:41:38 +0300

    changed paddings

--- script/main.js
+++ script/main.js
@@ -3,7 +3,7 @@ const listDocs = document.querySelector('.list-content');
 const renderData = (data) => {
     data.forEach(el => {
         listDocs.insertAdjacentHTML('beforeend',`
-        <div class=" flex flex-nowrap justify-between items-center gap-1 h-fit font-sm cursor-pointer text-left text-sm w-full hover:bg-sky-100">
+        <div class="px-4 flex flex-nowrap justify-between items-center gap-1 h-fit font-sm cursor-pointer text-left text-sm w-full hover:bg-sky-100">
             <p class="min-h-fit break-words w-2/5 text-justify px-4 line-clamp-3">${el.text}</p>
             <span class="break-words p-4 w-1/5">${el.data}</span>
             <span class="break-words w-1/5 flex flex-row flex-nowrap items-center gap-2 p-4">${el.size} ${el.includeImg ? '<i class="text-sky-700 fa-regular fa-file-image"></i>' : ''}</span>
