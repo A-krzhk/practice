275b6460163f76f7dec4eb2025646385333f840d
Alena koryazhkina <alena-koryazhkina@mail.ru>
2023-04-05 12:24:25 +0300

    added sort by data with bubble sort,  and done task 2

--- script/main.js
+++ script/main.js
@@ -1,19 +1,65 @@
 const listDocs = document.querySelector('.list-content');
 
+// Вспомогательная функция поставить один хтмл элемент рядом с другим
+const insertAfter = (elem, refElem) => refElem.parentNode.insertBefore(elem, refElem.nextSibling)
+
+// Вспомогательная функция по преобразованию даты в милисекунды из чч.мм.гг формата в приемлимый Date() формат гг.мм.чч
+const dataFormat = (date) =>  Date.parse(new Date(date.split('.').reverse().join('.')))
+
+// Вспомогательная функция для добавления класса активности 
+const activeClass = (cssClass, arrElem, activeEl) => {
+    arrElem.forEach(item => {
+        item.classList.remove(cssClass)
+    })
+    activeEl.classList.add(cssClass)
+}
+
+// Функция сортировки пузырьком
+const dataSort = (wrapperSelector, type) => {
+    
+    // Реализация смены цвета текста у активной сортировки
+    const arrHeaders = document.querySelectorAll('.header-list')
+    arrHeaders.forEach(item => {
+        if (item.classList.contains(type)) {
+            activeClass('text-sky-700', arrHeaders, item)   
+        }
+    })
+
+    // Реализация самой сортировки
+    const wrapper = document.querySelector(wrapperSelector)
+    const list = wrapper.children
+    for (let i = 0; i < list.length; i++) {
+        for (j = i; j < list.length; j++) {
+            if (+list[i].getAttribute(type) < +list[j].getAttribute(type)) {
+                const replaceNode = wrapper.replaceChild(list[j], list[i])
+                insertAfter(replaceNode, list[i])
+            }
+        }
+    }
+}
+
+// Функция для отрисовки данных из бд
 const renderData = (data) => {
     data.forEach(el => {
         listDocs.insertAdjacentHTML('beforeend',`
-        <div class="px-4 flex flex-nowrap justify-between items-center gap-1 h-fit font-sm cursor-pointer text-left text-sm w-full hover:bg-sky-100">
-            <p class="min-h-fit break-words w-2/5 text-justify px-4 line-clamp-3">${el.text}</p>
-            <span class="break-words p-4 w-1/5">${el.data}</span>
-            <span class="break-words w-1/5 flex flex-row flex-nowrap items-center gap-2 p-4">${el.size} ${el.includeImg ? '<i class="text-sky-700 fa-regular fa-file-image"></i>' : ''}</span>
-            <span class="break-words p-4 w-1/5">${el.author}</span>
+        <div data-id="${el.id}" data-date="${dataFormat(el.data)}" data-lang="${el.language}" data-author="${el.author}">
+            <div class="px-4 flex flex-nowrap justify-between items-start gap-1 h-fit font-sm cursor-pointer text-left text-sm w-full hover:bg-sky-100">
+                <p class="min-h-fit break-words w-2/6 text-justify px-4 line-clamp-3">${el.text}</p>
+                <span class="break-words px-4 w-1/6">${el.data}</span>
+                <span class="break-words w-1/6 flex flex-row flex-nowrap items-center gap-2 px-4">${el.size} ${el.includeImg ? '<i class="text-slate-700 fa-regular fa-file-image"></i>' : ''}</span>
+                <span class="break-words px-4 w-1/6">${el.author}</span>
+                <span class="break-words px-4 w-1/6">${el.language}</span>
+            </div>
+            <hr class="mt-5">
         </div>
-        <hr>
         `);
     });
 }
 
+// windowState - функция, которую напишет dev_Grigoriy
+const windowState = (e) => {console.log(e.target)}
+
+// Функция получения данных из бд
 const getData = (url) => {
     fetch(url)
         .then((response) => {
@@ -25,6 +71,14 @@ const getData = (url) => {
         })
         .then((data) => {
             renderData(data);
+
+            // Сортировка по умолчанию = сортировка по дате
+            dataSort('.list-content', 'data-date')
+
+            // Вызов функции windowState, которую добавят из dev_Grigoriy
+            for(let i = 0; i < listDocs.children.length; i++){
+                listDocs.children[i].addEventListener('click', windowState)
+            }
         })
         .catch((error) => {
             console.error(error.message);
@@ -32,5 +86,6 @@ const getData = (url) => {
 };
 
 getData('../database/dbase.json')
+
 
 
