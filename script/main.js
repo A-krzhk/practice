93cab6666581b20180a46fbb9f2bfbfd3218a59c
Alena koryazhkina <alena-koryazhkina@mail.ru>
2023-04-11 00:08:25 +0300

    changed type of sorting

--- script/main.js
+++ script/main.js
@@ -1,23 +1,24 @@
-const listDocs = document.querySelector('.list-content');
+const listDocs = document.querySelector('.list-content'),
+      sortHeaders = document.querySelectorAll('.sort-header');
 let dataDB = [];
 
 // Вспомогательная функция по преобразованию даты в милисекунды из чч.мм.гг формата в приемлимый Date() формат гг.мм.чч
 const dataFormat = (date) =>  Date.parse(new Date(date.split('.').reverse().join('.')))
 
-// Вспомогательная функция для добавления класса активности 
-const activeClass = (cssClass, arrElem, activeEl) => {
+// Вспомогательная функция для добавления класса активности к сортировке
+const activeClass = (arrElem, activeItem, rotate='') => {
     arrElem.forEach(item => {
-        item.classList.remove(cssClass)
+        const elem = item.querySelector(`i`)
+        if (elem) elem.remove()
     })
-    activeEl.classList.add(cssClass)
+    activeItem.insertAdjacentHTML('beforeend', `<i class="fa-solid fa-angle-up text-slate-700 ${rotate}"></i>`)
 }
 
-
 // Функция для отрисовки данных из бд
-const renderData = (data) => {
+const renderData = (data,directionSortStr) => {
     data.forEach(el => {
-        listDocs.insertAdjacentHTML('beforeend',`
-        <div data-id="${el.id}" data-date="${dataFormat(el.data)}" data-lang="${el.language}" data-author="${el.author}">
+        listDocs.insertAdjacentHTML(directionSortStr,`
+        <div data-id="${el.id}">
             <div class="p-4 flex flex-nowrap justify-between items-start gap-1 h-fit font-sm cursor-pointer text-left text-sm w-full hover:bg-sky-100">
                 <p class="min-h-fit break-words w-2/6 text-justify px-4 line-clamp-3">${el.text}</p>
                 <span class="break-words px-4 w-1/6">${el.data}</span>
@@ -31,8 +32,29 @@ const renderData = (data) => {
     });
 }
 
-// windowState - функция, которую напишет dev_Grigoriy
-const windowState = (e) => {console.log(e.target)}
+// Функция для сортировки данных по убыванию
+const sortData = (arr, field, directionSortStr) => {
+    let sorted;
+    if (field=="data") {
+        sorted = arr.sort((a, b) => dataFormat(b[field]) - dataFormat(a[field]));    
+    }
+    else if(field=="size") {
+        sorted = arr.sort((a, b) => parseFloat(b[field]) - parseFloat(a[field]));
+    } 
+    else {
+        sorted = arr.sort((a, b) => a[field].localeCompare(b[field]));
+    }
+    renderData(sorted, directionSortStr)
+}
+
+// Навешивание функции сортировки на все пункты заголовка
+sortHeaders.forEach(item => {
+    item.addEventListener('click', () => {
+        activeClass(sortHeaders, item, 'rotate-180')
+        listDocs.innerHTML = ''
+        sortData(dataDB, item.dataset.sort, 'beforeend')
+    })
+})
 
 // Функция получения данных из бд
 const getData = (url) => {
@@ -45,20 +67,10 @@ const getData = (url) => {
             }
         })
         .then((data) => {
-            // Сортировка по умолчанию = сортировка по дате
-            data.sort((a, b) => dataFormat(b.data) - dataFormat(a.data));
-            // Отрисовка списка
-            renderData(data);
+            // Сортировка по умолчанию = сортировка по дате (новое сверху) + отрисовка на странице
+            sortData(data, "data",'beforeend')
 
-            // Вызов функции windowState, которую добавят из dev_Grigoriy
-            for(let i = 0; i < listDocs.children.length; i++){
-                listDocs.children[i].addEventListener('click', windowState)
-            }
-            return data;
-        })
-        .then(data => {
             dataDB = [...data]
-            console.log(dataDB)
         })
         .catch((error) => {
             console.error(error.message);
