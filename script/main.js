a8e1a20f43119bb360ef42f32fa38b72b33340a5
Alena koryazhkina <alena-koryazhkina@mail.ru>
2023-04-10 15:26:35 +0300

    delete bubble sort

--- script/main.js
+++ script/main.js
@@ -1,7 +1,5 @@
 const listDocs = document.querySelector('.list-content');
-
-// Вспомогательная функция поставить один хтмл элемент рядом с другим
-const insertAfter = (elem, refElem) => refElem.parentNode.insertBefore(elem, refElem.nextSibling)
+let dataDB = [];
 
 // Вспомогательная функция по преобразованию даты в милисекунды из чч.мм.гг формата в приемлимый Date() формат гг.мм.чч
 const dataFormat = (date) =>  Date.parse(new Date(date.split('.').reverse().join('.')))
@@ -14,43 +12,20 @@ const activeClass = (cssClass, arrElem, activeEl) => {
     activeEl.classList.add(cssClass)
 }
 
-// Функция сортировки пузырьком
-const dataSort = (wrapperSelector, type) => {
-    
-    // Реализация смены цвета текста у активной сортировки
-    const arrHeaders = document.querySelectorAll('.header-list')
-    arrHeaders.forEach(item => {
-        if (item.classList.contains(type)) {
-            activeClass('text-sky-700', arrHeaders, item)   
-        }
-    })
-
-    // Реализация самой сортировки
-    const wrapper = document.querySelector(wrapperSelector)
-    const list = wrapper.children
-    for (let i = 0; i < list.length; i++) {
-        for (j = i; j < list.length; j++) {
-            if (+list[i].getAttribute(type) < +list[j].getAttribute(type)) {
-                const replaceNode = wrapper.replaceChild(list[j], list[i])
-                insertAfter(replaceNode, list[i])
-            }
-        }
-    }
-}
 
 // Функция для отрисовки данных из бд
 const renderData = (data) => {
     data.forEach(el => {
         listDocs.insertAdjacentHTML('beforeend',`
         <div data-id="${el.id}" data-date="${dataFormat(el.data)}" data-lang="${el.language}" data-author="${el.author}">
-            <div class="px-4 flex flex-nowrap justify-between items-start gap-1 h-fit font-sm cursor-pointer text-left text-sm w-full hover:bg-sky-100">
+            <div class="p-4 flex flex-nowrap justify-between items-start gap-1 h-fit font-sm cursor-pointer text-left text-sm w-full hover:bg-sky-100">
                 <p class="min-h-fit break-words w-2/6 text-justify px-4 line-clamp-3">${el.text}</p>
                 <span class="break-words px-4 w-1/6">${el.data}</span>
                 <span class="break-words w-1/6 flex flex-row flex-nowrap items-center gap-2 px-4">${el.size} ${el.includeImg ? '<i class="text-slate-700 fa-regular fa-file-image"></i>' : ''}</span>
                 <span class="break-words px-4 w-1/6">${el.author}</span>
                 <span class="break-words px-4 w-1/6">${el.language}</span>
             </div>
-            <hr class="mt-5">
+            <hr class="">
         </div>
         `);
     });
@@ -70,15 +45,20 @@ const getData = (url) => {
             }
         })
         .then((data) => {
-            renderData(data);
-
             // Сортировка по умолчанию = сортировка по дате
-            dataSort('.list-content', 'data-date')
+            data.sort((a, b) => dataFormat(b.data) - dataFormat(a.data));
+            // Отрисовка списка
+            renderData(data);
 
             // Вызов функции windowState, которую добавят из dev_Grigoriy
             for(let i = 0; i < listDocs.children.length; i++){
                 listDocs.children[i].addEventListener('click', windowState)
             }
+            return data;
+        })
+        .then(data => {
+            dataDB = [...data]
+            console.log(dataDB)
         })
         .catch((error) => {
             console.error(error.message);
@@ -86,6 +66,4 @@ const getData = (url) => {
 };
 
 getData('../database/dbase.json')
-
-
 
